9fea17b6ba7e1ed0d2442b49c043032578330565
JoshuaBRussell <theruss007@gmail.com>
2022-05-06 16:57:37 -0700

    Add non-blocking keyboard input.

--- SimpleGame/src/Ship.c
+++ SimpleGame/src/Ship.c
@@ -5,7 +5,53 @@
 #include "stdio.h"
 
 
-enum {BSP_TICKS_PER_SEC = 100};
+
+
+//Key Press
+#include <sys/select.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define NB_DISABLE 1
+#define NB_ENABLE 0
+
+
+int kbhit(){
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+void nonblock(int state)
+{
+    struct termios ttystate;
+
+    //get the terminal state
+    tcgetattr(STDIN_FILENO, &ttystate);
+
+    if (state==NB_ENABLE)
+    {
+        //turn off canonical mode
+        ttystate.c_lflag &= ~ICANON;
+        //minimum of number input read.
+        ttystate.c_cc[VMIN] = 1;
+    }
+    else if (state==NB_DISABLE)
+    {
+        //turn on canonical mode
+        ttystate.c_lflag |= ICANON;
+    }
+    //set the terminal attributes.
+    tcsetattr(STDIN_FILENO, TCSANOW, &ttystate);
+
+}
+
+enum {BSP_TICKS_PER_SEC = 2};
 
 
 //Declare Ship Active Object
@@ -53,7 +99,30 @@ static QState Ship_Active(Ship * const me, QEvt const * const e){
         }
 
         case TIME_SIG: {
-            printf("Timer Event...\n");
+
+            char c;
+            //Detect Key Press
+            nonblock(NB_ENABLE);
+            int i = kbhit();
+            if(i != 0){
+               c = fgetc(stdin);
+            }
+            nonblock(NB_DISABLE);
+
+            if (c == 'w'){
+                printf("Move Up\n");
+            } 
+            if (c == 'a'){
+                printf("Move Left\n");
+            }
+            if (c == 's'){
+                printf("Move Down\n");
+            } 
+            if (c == 'd'){
+                printf("Move Right\n");
+            }
+
+
             status = Q_HANDLED();
             break;
         }
